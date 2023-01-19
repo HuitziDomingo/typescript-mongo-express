cc2dcb2df9eff77f857995a9bc7c54666d14b4c5
HuitziDomingo <huitzitzilidomingo@gmail.com>
2023-01-19 15:30:00 -0600

    conexion a mongoDB agregada

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response } from 'express'
-// import mongoose from 'mongoose'
+import mongoose from 'mongoose'
 
 const app = express()
-
+mongoose.set('strictQuery', false)
 
 app.listen(4000, () => console.log('Hola EXPRESS'))
-// mongoose.connect('mongodb://127.0.0.1:27017/mongo-typescript')
+mongoose.connect('mongodb://localhost:27017/mongo-typescript')
 
 
 app.get('/', (req: Request, res: Response) => {
